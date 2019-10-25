4ac345288bf29d0b81f4060b425b6d43abb2b6ea
Moralis Tsai <moralis.tsai@gmail.com>
2019-10-25 21:47:10 +0800

    Add reset vendor css

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { Reset } from './styles/vendor/reset';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <App />
+    <Reset />
+  </>
+  , document.getElementById('root'));
